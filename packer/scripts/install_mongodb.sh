33a9eaed3435693c62245e122b847de1b450f571
Andrey Egupov <oksid_d_ant@mail.ru>
2023-12-19 14:27:13 +0300

    HW_9. Terraform2. Terraform stage and prod. Egupov Andrey

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
+echo "================apt update ================"
+apt update
+echo "================sleep ================"
+sleep 10
 echo "============= install mongodb ================="
 apt install -y mongodb
--- terraform/prod/variables.tf
+++ terraform/prod/variables.tf
@@ -6,11 +6,9 @@ variable "folder_id" {
 }
 variable "zone" {
   description = "Zone"
-  # Значение по умолчанию
-  default = "ru-central1-a"
+  default     = "ru-central1-a"
 }
 variable "public_key_path" {
-  # Описание переменной
   description = "Path to the public key used for ssh access"
 }
 variable "private_key_path" {
@@ -25,4 +23,12 @@ variable "subnet_id" {
 }
 variable "service_account_key_file" {
   description = "key .json"
+}
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+variable "db_disk_image" {
+  description = "Disk image for reddit db"
+  default     = "reddit-db-base"
 }
